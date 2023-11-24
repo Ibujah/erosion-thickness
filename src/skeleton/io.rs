d5cf0454948e6fcdb1ea0567f78a6250d4f43837
Bastien <dbastien71@gmail.com>
2023-11-24 13:12:47 +0100

    Read and write ply files

--- src/skeleton/io.rs
+++ src/skeleton/io.rs
@@ -1,137 +1,105 @@
+use anyhow::Result;
+use std::collections::HashMap;
 use std::fs::File;
-use std::io::{BufRead, BufReader, Write};
 
 use nalgebra::base::*;
 
+use ply_rs::parser::Parser;
+use ply_rs::ply::{Addable, DefaultElement, Encoding, Ply, Property};
+use ply_rs::writer::Writer;
+
 use crate::skeleton::skeleton;
 
-pub fn export_to_ply(skel: &skeleton::Skeleton, file_path: &str) -> std::io::Result<()> {
-    let mut file = File::create(file_path)?;
+pub fn import_from_ply(file_path: &str) -> Result<()> {
+    let mut f = std::fs::File::open(file_path).unwrap();
 
-    writeln!(file, "ply")?;
-    writeln!(file, "format ascii 1.0")?;
-    writeln!(file, "element vertex {}", skel.vertices.len())?;
-    writeln!(file, "property float x")?;
-    writeln!(file, "property float y")?;
-    writeln!(file, "property float z")?;
-    writeln!(file, "element edge {}", skel.edges.len())?;
-    writeln!(file, "property int vertex1")?;
-    writeln!(file, "property int vertex2")?;
-    writeln!(file, "element face {}", skel.faces.len())?;
-    writeln!(file, "property list uchar int vertex_indices")?;
-    writeln!(file, "end_header")?;
+    let p = Parser::<DefaultElement>::new();
+    let ply = p.read_ply(&mut f)?;
 
-    for vertex in &skel.vertices {
-        writeln!(file, "{} {} {}", vertex[0], vertex[1], vertex[2])?;
-    }
-
-    for edge in &skel.edges {
-        writeln!(file, "{} {}", edge[0], edge[1])?;
-    }
-
-    for face in &skel.faces {
-        let mut vertex_indices = Vec::new();
-        for &edge_index in face {
-            let [v1, v2] = skel.edges[edge_index];
-            vertex_indices.push(v1);
-            vertex_indices.push(v2);
-        }
-        vertex_indices.sort();
-        vertex_indices.dedup();
-        writeln!(
-            file,
-            "3 {} {} {}",
-            vertex_indices[0], vertex_indices[1], vertex_indices[2]
-        )?;
-    }
-
-    Ok(())
-}
-
-pub fn export_to_obj(skel: &skeleton::Skeleton, file_path: &str) -> std::io::Result<()> {
-    let mut file = File::create(file_path)?;
-
-    for vertex in &skel.vertices {
-        writeln!(file, "v {} {} {}", vertex[0], vertex[1], vertex[2])?;
-    }
-
-    for face in &skel.faces {
-        let mut vertex_indices = Vec::new();
-        for &edge_index in face {
-            let [v1, v2] = skel.edges[edge_index];
-            vertex_indices.push(v1);
-            vertex_indices.push(v2);
-        }
-        vertex_indices.sort();
-        vertex_indices.dedup();
-        writeln!(
-            file,
-            "f {} {} {}",
-            vertex_indices[0] + 1,
-            vertex_indices[1] + 1,
-            vertex_indices[2] + 1
-        )?;
-    }
-
-    Ok(())
-}
-
-pub fn import_from_obj(file_path: &str) -> std::io::Result<skeleton::Skeleton> {
     let mut skel = skeleton::Skeleton::new();
-    let file = File::open(file_path)?;
-    let reader = BufReader::new(file);
 
-    for line in reader.lines() {
-        let line = line?;
-        let mut parts = line.split_whitespace();
+    // load vertices
+    if !ply.payload.contains_key("vertex") {
+        return Err(anyhow::Error::msg("No vertex element in file"));
+    }
+    for v in ply.payload["vertex"].iter() {
+        let mut x = None;
+        let mut y = None;
+        let mut z = None;
+        let mut radius = None;
+        let mut properties = HashMap::new();
 
-        match parts.next() {
-            Some("v") => {
-                let x: f32 = parts.next().unwrap().parse().unwrap();
-                let y: f32 = parts.next().unwrap().parse().unwrap();
-                let z: f32 = parts.next().unwrap().parse().unwrap();
-                skel.add_vertex(Vector3::new(x, y, z));
-                skel.radii.push(1.0);
-            }
-            Some("f") => {
-                let mut vertex_indices: Vec<usize> = Vec::new();
-                for part in parts {
-                    let indices: Vec<usize> = part
-                        .split('/')
-                        .map(|index| index.parse().unwrap_or(0))
-                        .collect();
-                    let vertex_index = indices[0] - 1;
-                    vertex_indices.push(vertex_index);
+        for (key, &prop) in v.iter() {
+            match (key.as_ref(), prop) {
+                ("x", Property::Float(val)) => x = Some(val),
+                ("y", Property::Float(val)) => y = Some(val),
+                ("z", Property::Float(val)) => z = Some(val),
+                ("radius", Property::Float(val)) => radius = Some(val),
+                (k, p) => {
+                    properties.insert(k.to_string(), p);
+                    ()
                 }
-                let v1: usize = vertex_indices[0];
-                let v2: usize = vertex_indices[1];
-                let v3: usize = vertex_indices[2];
-
-                let e1 = skel.add_edge([v1, v2]);
-                let e2 = skel.add_edge([v2, v3]);
-                let e3 = skel.add_edge([v3, v1]);
-
-                skel.add_face([e1, e2, e3]);
             }
-            _ => continue,
         }
+        let x = x.ok_or(anyhow::Error::msg("No x property in vertex"))?;
+        let y = y.ok_or(anyhow::Error::msg("No y property in vertex"))?;
+        let z = z.ok_or(anyhow::Error::msg("No z property in vertex"))?;
+        let radius = radius.ok_or(anyhow::Error::msg("No radius property in vertex"))?;
+        skel.add_vertex(Vector3::new(x, y, z), radius, properties);
     }
 
-    Ok(skel)
+    // load faces
+    if !ply.payload.contains_key("face") {
+        return Err(anyhow::Error::msg("No face element in file"));
+    }
+    for f in ply.payload["face"].iter() {
+        let mut list_vertices = None;
+        let mut properties = HashMap::new();
+
+        for (key, &prop) in f.iter() {
+            match (key.as_ref(), prop) {
+                ("vertex_indices", Property::ListUInt(val)) => {
+                    list_vertices = Some(val.iter().map(|&v| usize::try_from(v).unwrap()).collect())
+                }
+                (k, p) => {
+                    properties.insert(k.to_string(), p);
+                    ()
+                }
+            }
+        }
+
+        let list_vertices: Vec<usize> =
+            list_vertices.ok_or(anyhow::Error::msg("No vertex_indices property in face"))?;
+
+        skel.add_face(list_vertices, properties);
+    }
+
+    Ok(())
 }
 
-pub fn import_radii(skel: &mut skeleton::Skeleton, file_path: &str) -> std::io::Result<()> {
-    let file = File::open(file_path)?;
-    let reader = BufReader::new(file);
+pub fn export_to_ply(skel: &skeleton::Skeleton, file_path: &str) -> Result<()> {
+    let mut ply = {
+        let mut ply = Ply::<DefaultElement>::new();
+        ply.header.encoding = Encoding::Ascii;
+        ply.header
+            .comments
+            .push("Generated with https://github.com/Ibujah/erosion-thickness".to_string());
 
-    skel.radii.clear();
-    for line in reader.lines() {
-        let line = line?;
-        let mut parts = line.split_whitespace();
+        ply.header.elements.add(skel.vertex_header_element());
+        ply.header.elements.add(skel.face_header_element());
 
-        let r: f32 = parts.next().unwrap().parse().unwrap();
-        skel.radii.push(r);
-    }
+        let vertices = skel.vertex_payload_element();
+        let faces = skel.face_payload_element();
 
+        ply.payload.insert("vertex".to_string(), vertices);
+        ply.payload.insert("face".to_string(), faces);
+
+        ply.make_consistent().unwrap();
+        ply
+    };
+
+    let mut file = File::create(file_path)?;
+    let w = Writer::new();
+    let written = w.write_ply(&mut file, &mut ply).unwrap();
     Ok(())
 }
--- src/skeleton/skeleton.rs
+++ src/skeleton/skeleton.rs
@@ -1,30 +1,144 @@
 use nalgebra::base::*;
 use std::collections::HashMap;
 
+use ply_rs::ply::{
+    Addable, DefaultElement, ElementDef, Property, PropertyDef, PropertyType, ScalarType,
+};
+
 pub struct Skeleton {
-    pub(super) vertices: Vec<Vector3<f32>>,
-    pub(super) radii: Vec<f32>,
-    pub(super) edges: Vec<[usize; 2]>,
-    pub(super) faces: Vec<[usize; 3]>,
-    pub(super) vertex_to_edges: HashMap<usize, Vec<usize>>,
-    pub(super) edge_to_faces: HashMap<usize, Vec<usize>>,
+    vertex_property_types: HashMap<String, PropertyType>,
+    face_property_types: HashMap<String, PropertyType>,
+    vertex_coords: Vec<Vector3<f32>>,
+    vertex_radius: Vec<f32>,
+    vertex_properties: Vec<HashMap<String, Property>>,
+    edges: Vec<[usize; 2]>,
+    face_vertices: Vec<Vec<usize>>,
+    face_edges: Vec<Vec<usize>>,
+    faces_properties: Vec<HashMap<String, Property>>,
+    vertex_to_edges: HashMap<usize, Vec<usize>>,
+    edge_to_faces: HashMap<usize, Vec<usize>>,
+}
+
+fn property_to_propertytype(prop: Property) -> PropertyType {
+    match prop {
+        Property::Char(_) => PropertyType::Scalar(ScalarType::Char),
+        Property::UChar(_) => PropertyType::Scalar(ScalarType::UChar),
+        Property::Short(_) => PropertyType::Scalar(ScalarType::Short),
+        Property::UShort(_) => PropertyType::Scalar(ScalarType::UShort),
+        Property::Int(_) => PropertyType::Scalar(ScalarType::Int),
+        Property::UInt(_) => PropertyType::Scalar(ScalarType::UInt),
+        Property::Float(_) => PropertyType::Scalar(ScalarType::Float),
+        Property::Double(_) => PropertyType::Scalar(ScalarType::Double),
+        Property::ListChar(_) => PropertyType::List(ScalarType::UChar, ScalarType::Char),
+        Property::ListUChar(_) => PropertyType::List(ScalarType::UChar, ScalarType::UChar),
+        Property::ListShort(_) => PropertyType::List(ScalarType::UChar, ScalarType::Short),
+        Property::ListUShort(_) => PropertyType::List(ScalarType::UChar, ScalarType::UShort),
+        Property::ListInt(_) => PropertyType::List(ScalarType::UChar, ScalarType::Int),
+        Property::ListUInt(_) => PropertyType::List(ScalarType::UChar, ScalarType::UInt),
+        Property::ListFloat(_) => PropertyType::List(ScalarType::UChar, ScalarType::Float),
+        Property::ListDouble(_) => PropertyType::List(ScalarType::UChar, ScalarType::Double),
+    }
 }
 
 impl Skeleton {
     pub fn new() -> Self {
         Skeleton {
-            vertices: Vec::new(),
-            radii: Vec::new(),
+            vertex_property_types: HashMap::new(),
+            face_property_types: HashMap::new(),
+            vertex_coords: Vec::new(),
+            vertex_radius: Vec::new(),
+            vertex_properties: Vec::new(),
             edges: Vec::new(),
-            faces: Vec::new(),
+            face_vertices: Vec::new(),
+            face_edges: Vec::new(),
+            faces_properties: Vec::new(),
             vertex_to_edges: HashMap::new(),
             edge_to_faces: HashMap::new(),
         }
     }
 
-    pub(super) fn add_vertex(&mut self, position: Vector3<f32>) -> usize {
-        self.vertices.push(position);
-        self.vertices.len() - 1
+    pub fn vertex_header_element(&self) -> ElementDef {
+        let mut vertex_element = ElementDef::new("vertex".to_string());
+        for (&key, &prop) in self.vertex_property_types.iter() {
+            vertex_element.properties.add(PropertyDef::new(key, prop));
+        }
+        vertex_element
+    }
+
+    pub fn vertex_payload_element(&self) -> Vec<DefaultElement> {
+        let mut vertices = Vec::new();
+
+        for i in 0..self.vertex_coords.len() {
+            let mut vertex = DefaultElement::new();
+            vertex.insert("x".to_string(), Property::Float(self.vertex_coords[i][0]));
+            vertex.insert("y".to_string(), Property::Float(self.vertex_coords[i][1]));
+            vertex.insert("z".to_string(), Property::Float(self.vertex_coords[i][2]));
+            vertex.insert("radius".to_string(), Property::Float(self.vertex_radius[i]));
+            for (&key, &val) in self.vertex_properties[i].iter() {
+                vertex.insert(key, val);
+            }
+            vertices.push(vertex);
+        }
+
+        vertices
+    }
+
+    pub fn face_header_element(&self) -> ElementDef {
+        let mut face_element = ElementDef::new("face".to_string());
+        for (&key, &prop) in self.face_property_types.iter() {
+            face_element.properties.add(PropertyDef::new(key, prop));
+        }
+        face_element
+    }
+
+    pub fn face_payload_element(&self) -> Vec<DefaultElement> {
+        let mut faces = Vec::new();
+
+        for i in 0..self.face_edges.len() {
+            let mut face = DefaultElement::new();
+            face.insert(
+                "vertex_indices".to_string(),
+                Property::ListUInt(
+                    self.face_vertices[i]
+                        .iter()
+                        .map(|&i| u32::try_from(i).unwrap())
+                        .collect(),
+                ),
+            );
+            for (&key, &val) in self.faces_properties[i].iter() {
+                face.insert(key, val);
+            }
+            faces.push(face);
+        }
+
+        faces
+    }
+
+    pub(super) fn add_vertex(
+        &mut self,
+        position: Vector3<f32>,
+        radius: f32,
+        properties: HashMap<String, Property>,
+    ) -> usize {
+        self.vertex_coords.push(position);
+        self.vertex_radius.push(radius);
+        self.vertex_properties.push(properties);
+        self.vertex_property_types
+            .insert("x".to_string(), PropertyType::Scalar(ScalarType::Float));
+        self.vertex_property_types
+            .insert("y".to_string(), PropertyType::Scalar(ScalarType::Float));
+        self.vertex_property_types
+            .insert("z".to_string(), PropertyType::Scalar(ScalarType::Float));
+        self.vertex_property_types.insert(
+            "radius".to_string(),
+            PropertyType::Scalar(ScalarType::Float),
+        );
+        for (key, prop) in properties {
+            let ptype = property_to_propertytype(prop);
+            self.vertex_property_types.insert(key, ptype);
+        }
+
+        self.vertex_coords.len() - 1
     }
 
     pub(super) fn add_edge(&mut self, vertex_indices: [usize; 2]) -> usize {
@@ -53,31 +167,53 @@ impl Skeleton {
         edge_index
     }
 
-    pub(super) fn add_face(&mut self, edge_indices: [usize; 3]) {
-        self.faces.push(edge_indices);
+    pub(super) fn add_face(
+        &mut self,
+        vertex_indices: Vec<usize>,
+        properties: HashMap<String, Property>,
+    ) {
+        let mut edge_indices = Vec::new();
+        for i in 0..vertex_indices.len() {
+            let j = (i + 1) % vertex_indices.len();
+            let vi = vertex_indices[i];
+            let vj = vertex_indices[j];
+            let ei = self.add_edge([vi, vj]);
+            edge_indices.push(ei);
+        }
+        self.face_vertices.push(vertex_indices);
+        self.face_edges.push(edge_indices);
+        self.faces_properties.push(properties);
+        self.face_property_types.insert(
+            "vertex_indices".to_string(),
+            PropertyType::List(ScalarType::UChar, ScalarType::UInt),
+        );
+        for (key, prop) in properties {
+            let ptype = property_to_propertytype(prop);
+            self.face_property_types.insert(key, ptype);
+        }
 
         for edge_index in &edge_indices {
             self.edge_to_faces
                 .entry(*edge_index)
                 .or_insert(Vec::new())
-                .push(self.faces.len() - 1);
+                .push(self.face_edges.len() - 1);
         }
     }
 
     pub fn get_vertices(&self) -> &Vec<Vector3<f32>> {
-        &self.vertices
+        &self.vertex_coords
     }
 
     pub fn get_radii(&self) -> &Vec<f32> {
-        &self.radii
+        &self.vertex_radius
     }
 
     pub fn get_edges(&self) -> &Vec<[usize; 2]> {
         &self.edges
     }
 
-    pub fn get_faces(&self) -> &Vec<[usize; 3]> {
-        &self.faces
+    pub fn get_faces(&self) -> &Vec<Vec<usize>> {
+        &self.face_edges
     }
 
     pub fn get_edges_from_vertex(&self, vertex_index: usize) -> Option<&Vec<usize>> {
